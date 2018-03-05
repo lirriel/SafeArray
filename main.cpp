8de388218a4ca2cac7ac480de4bce622e584a555
lirriel <druidmif@gmail.com>
2018-03-05 23:12:39 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,120 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+// Module Name:  main.cpp
+// Authors:      Sergey Shershakov
+// Version:      0.2.0
+// Date:         30.01.2017
+//
+// This is a part of the course "Algorithms and Data Structures" 
+// provided by  the School of Software Engineering of the Faculty 
+// of Computer Science at the Higher School of Economics.
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+
+#include "safearray.h"
+#include "subject.h"
+
+
+using namespace std;
+
+static const char* TEST_FNAME = "D:\\HSE\\C++\\3_project\\prj-1.1\\prj\\res\\raw\\subjects1.txt";
+static const int SUBS_NUM = 1;
+
+void loadSubjects(const char* fn, xi::SafeArray<xi::Subject>& subjs)
+{
+    using namespace std;
+
+    // пытаемся открыть файл для чтения
+    ifstream inf(fn);
+    bool fileNotOpen = !inf;
+//    ASSERT_FALSE(fileNotOpen);
+
+    // пытаемся вычитать текстовый файл строку за строкой
+    try
+    {
+        for (int i = 0; i < SUBS_NUM; i++)
+        {
+            inf >> subjs[i];
+        }
+    }
+    catch (out_of_range e)
+    {
+        cerr << "Error reading file" << fn << endl;
+    }
+
+    inf.close();
+}
+
+int main()
+{
+    using namespace xi;
+
+    cout << "Hello, World!" << endl;
+
+
+    // Simple safearray
+//    SafeArray<int> sa;
+
+    //my tests!
+    SafeArray<int> sa(3);
+    bool f1 = sa.getRawPtr() == nullptr;
+    bool f2 = sa.getCapacity() == 3;
+    cout<<f1<<" "<<f2<<endl;
+    sa[0] = 7;
+    sa[1] = 2;
+    sa[2] = 6;
+//    SafeArray<int> sd = sa;
+//    bool f = sa.getRawPtr() == sd.getRawPtr();
+//    cout<<f;
+//    const SafeArray<int> t = *sa;
+    SafeArray<int> sd(sa);
+    sa.~SafeArray();
+//
+    cout<<sd.getCapacity()<<endl;
+    for (int i = 0; i < sd.getCapacity(); ++i) {
+        cout<<sd[i]<<" ";
+    }
+//    f1 = sa[0] == 7;
+//    const int el = sa[1];
+//    f2 == el == sa[1];
+//    cout<<f1<<" "<<f2<<endl;
+//
+//    SafeArray<int> sb(1);
+//    f1 = sb.getRawPtr() == nullptr;
+//    f2 = sb.getCapacity() == 1;
+//    cout<<f1<<" "<<f2<<endl;
+//    SafeArray<int> sc = sb;
+//    f1 = sc.getRawPtr() == nullptr;
+//    f2 = sc.getCapacity() == 1;
+//    cout<<f1<<" "<<f2<<endl;
+    //my tests!
+
+    //MY TEST
+//    Subject s;
+//    xi::SafeArray<xi::Subject> subjs(SUBS_NUM);
+//    loadSubjects(TEST_FNAME, subjs);
+//
+//    /*
+//    EXPECT_EQ(subjs[1].name, "SSD2");
+//    EXPECT_EQ(subjs[1].title, "Introduction to Computer Systems");
+//
+//    // след. две строки эквивалентны
+//    EXPECT_EQ(subjs[1].description.getCapacity(), subjs[1].MAX_LINES);
+//    EXPECT_EQ(subjs[1].description.getCapacity(), xi::Subject::MAX_LINES);
+//     */
+////    bool f1 = subjs[1].name == "SSD2";
+////    bool f2 = subjs[1].title == "Introduction to Computer Systems";
+////    cout<<f1<<" "<<f2<<endl;
+////
+////    f1 = subjs[1].description.getCapacity() == subjs[1].MAX_LINES;
+////    f2 = subjs[1].description.getCapacity() == xi::Subject::MAX_LINES;
+////    cout<<f1<<" "<<f2<<endl;
+//
+//    for (int i = 0; i < SUBS_NUM; ++i) {
+//        cout<<subjs[i];
+//    }
+
+    //MY TEST
+    return 0;
+}
---
+++ safearray.hpp
@@ -0,0 +1,102 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief     Safearray class methods definition
+/// \author    Lyaysan Iskhakova
+/// \version   0.2.0
+/// \date      31.01.2017
+///            This is a part of the course "Algorithms and Data Structures" 
+///            provided by  the School of Software Engineering of the Faculty 
+///            of Computer Science at the Higher School of Economics.
+///
+/// This file contains templates of the method of the class Safearray.
+/// This file is not going to be used w/o its main counterpart, safearray.h.
+/// Since the file is used only through safearray.h, which has its own "include 
+/// watcher", we add no addition "watcher" here.
+///
+////////////////////////////////////////////////////////////////////////////////
+
+// #ifndef    // No need to have a watcher here!
+
+
+#include "safearray.h"
+
+
+// we have to indicate that methods of the class Safearray are also inside the namespace xi
+namespace xi {
+
+
+template <class T>
+SafeArray<T>::SafeArray(size_t cap)
+{
+    // TODO: этот метод надо дополнить
+    _capacity = cap;
+    _storage = new T[_capacity];
+}
+
+// TODO: здесь должно быть еще несколько методов
+
+
+template <class T>
+SafeArray<T>::~SafeArray()
+{
+    delete[] _storage;
+    _storage = nullptr;
+}
+
+
+template <class T>
+T& SafeArray<T>::operator[](size_t k)
+{
+    if (k >= _capacity)
+        throw std::out_of_range("Index is out of range!");
+
+    return _storage[k];
+}
+
+
+template <class T>
+const T & SafeArray<T>::operator[](size_t k) const
+{
+    checkBounds(k);
+    return _storage[k];
+}
+
+
+template <class T>
+size_t SafeArray<T>::getCapacity() const
+{
+    return _capacity;
+}
+
+
+template <class T>
+void SafeArray<T>::checkBounds(size_t index) const
+{
+    if (index >= _capacity)
+        throw std::out_of_range("Index is out of range!");
+}
+
+
+template <class T>
+SafeArray<T>::SafeArray(const SafeArray& array)
+{
+    *this = array;
+}
+
+
+template <class T>
+SafeArray<T>& SafeArray<T>::operator=(const SafeArray& safeArray)
+{
+    _capacity = safeArray._capacity;
+
+    delete[] _storage;
+    _storage = new T[_capacity];
+    for (int i = 0; i < _capacity; ++i)
+    {
+        _storage[i] = safeArray._storage[i];
+    }
+    return *this;
+}
+
+
+} // namespace xi
